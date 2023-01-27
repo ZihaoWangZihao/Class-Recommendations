d7f022e79bc86dbd0ccc0dc5ab2935194916dce6
Zihao Wang <zihaowangzihao@gmail.com>
2023-01-27 09:46:59 -0500

    Person class

--- data.py
+++ data.py
@@ -1,17 +1,19 @@
-class dp(object):
+class Course(object):
     """
-    Represents a datapoint(dp)
+    Represents a Course
 
     course_num    <int>
     semester_offered   <string>
     rating     <int>
     hours     <int>
+    units   <int>
     """
-    def __init__(self, course_num, semester_offered, rating, hours):
+    def __init__(self, course_num, semester_offered, rating, hours, units):
         self.course_num = course_num
         self.semester_offered = semester_offered
         self.rating = rating
         self.hours = hours
+        self.units = units
     """ Field Accessors """
     def get_course_num(self):
         return self.course_num
@@ -21,3 +23,35 @@ class dp(object):
         return self.rating
     def get_hours(self):
         return self.hours
+    def get_units(self):
+        return self.units
+
+class Person(object):
+    """
+    Represents a Person
+
+    name    <string>
+    year   <string>
+    major     <major>
+    career     <major>
+    courses   <list with each element of type Course>
+    """
+    def __init__(self, name, year, major, career, courses):
+        self.name = name
+        self.year = year
+        self.major = major
+        self.career = career
+        self.courses = courses
+    """ Field Accessors """
+    def get_name(self):
+        return self.name
+    def get_year(self):
+        return self.year
+    def get_major(self):
+        return self.major
+    def get_career(self):
+        return self.career
+    def get_courses(self):
+        return self.courses
+
+
